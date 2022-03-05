f2089a52da58b9084ad6efcd07232fae0dede693
Mike Williams <mikey32905@gmail.com>
2022-03-05 10:24:20 -0500

    added in CollectionsController, MovieCollectionController, and ActorsController.

---
+++ MovieProDemo/Controllers/ActorsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieProDemo.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace MovieProDemo.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _mappingService;
+
+
+        public ActorsController(IRemoteMovieService tmdbMovieService, IDataMappingService mappingService)
+        {
+            _tmdbMovieService = tmdbMovieService;
+            _mappingService = mappingService;
+        }
+
+
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var actor = await _tmdbMovieService.ActorDetailAsync(id);
+            actor = _mappingService.MapActorDetail(actor);
+            return View(actor);
+        }
+    }
+}
